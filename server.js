c052bce6f9528f38a5f172bceb7eb951be9be29f
James <tytechortz@gmail.com>
2018-10-13 02:01:52 -0600

    adds models folder

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app     = express();
 
+const commieController = require('./controllers/fruits');
+
+
+
+app.use('/commies', commieController);
+
+app.get('/', (req, res) => {
+    res.send('This is Commie app')
+});
+
+
 app.listen(3000, () => {
     console.log('listening on port 3000')
 });
