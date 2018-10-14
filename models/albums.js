0961fe8e0393f5e6a92c551dc7aaa8ef85de835d
James <tytechortz@gmail.com>
2018-10-13 19:20:49 -0600

    adds views and show files, moves data to models

--- models/albums.js
+++ models/albums.js
@@ -1,11 +1,34 @@
 // this is the data, or model
 const mongoose = require('mongoose');
 
-const bicycleSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    artist: {type: String, required: true},
-    genre: {type: String, required: true},
-    year: Number,
-});
+const albums = [
+    {
+        name:'Thriller',
+        artist: 'MJ',
+        genre: 'Pop',
+        year: 1984 
+     },
+     {
+        name:'Rubber Soul',
+        artist: 'Beatles',
+        genre: 'Rock',
+        year: 1965
+     },
+     {
+        name:'Duke Live at Newport',
+        artist: 'Duke Ellington',
+        genre: 'Jazz',
+        year: 1958 
+     }
+    ];
 
-module.exports = mongoose.model('Album', albumSchema);
+module.export = albums;
+
+// const albumSchema = new mongoose.Schema({
+//     name: {type: String, required: true},
+//     artist: {type: String, required: true},
+//     genre: {type: String, required: true},
+//     year: Number,
+// });
+
+//module.exports = mongoose.model('Album', albumSchema);
--- server.js
+++ server.js
@@ -8,9 +8,34 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //app.use('/albums', commieController);
 
+
+// Requiring model
+const Albums = require('./models/albums');
+     
+
+
+//index route
+app.get('/albums/', (req, res) => {
+    res.send(albums);
+});
+
+
+//show route
+// app.get('/albums/:index', (req, res) => {
+//     res.send(albums[req.params.index]);
+// });
+
+app.get('/albums/:index', (req, res) => {
+         res.render('show.ejs');
+});
+
+
+
 app.get('/', (req, res) => {
     res.send('This is Albums app')
 });
+
+
 
 
 app.listen(3000, () => {
